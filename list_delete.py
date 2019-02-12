8c33948cc2a0d4b1a711f6d014ddcaf12111c513
zorroyep <wds.1990@163.com>
2019-02-12 11:01:23 +0800

    test

---
+++ list_delete.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*- 
+
+list = [1,2,3,4,5]
+for i in list:
+    list.remove(list[i])
+
+print(list)
---
+++ stringVar_test.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super.__init__()
+        self.var = tk.StringVar()
+        self.
