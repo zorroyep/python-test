de179de4559b9f299ceccec98cd2d02737b2b42e
zorroyep <wds.1990@163.com>
2019-02-19 21:41:33 +0800

    修改一

--- testTkinter.py
+++ testTkinter.py
@@ -8,29 +8,42 @@ root = tkinter.Tk()
 #
 class Application(tkinter.Frame):
     def chg_telnet(self):
-        self.telnet_inter = TelnetInterface(self)
-        self.telnet_inter.pack()
-        self.telnet_text = tkinter.Label(text="我是telnet界面")
-        self.telnet_text.pack()
+        if self.frame_2:
+            self.frame_2.destroy()
+        elif self.frame_1:
+            self.frame_1.pack()
+        else:
+            self.frame_1 = TelnetInterface(self)
+            self.frame_1.pack()
+
+        
 
     def chg_console(self):
-        self.console_inter = ConsoleInterface(self)
-        self.console_inter.pack()
-        self.telnet_text = tkinter.Label(text="我是Console界面")
-        self.telnet_text.pack()
+        if self.frame_1:
+            self.frame_1.destroy()
+        elif self.frame_2:
+            #self.frame_2 = ConsoleInterface(self)
+            self.frame_2.pack()
+        else:
+            self.frame_2 = ConsoleInterface(self)
+            self.frame_2.pack()
+        
 
 
     def __init__(self,master):
         super().__init__()
         self.pack()
-        self.telnet_btn = tkinter.Button(self,text="Telnet",command=self.chg_telnet)
+        self.telnet_btn = tkinter.Button(self,text="Telnet",command=lambda:self.chg_telnet())
 
         self.telnet_btn.pack()
-        self.console_btn = tkinter.Button(self,text="串口",command=self.chg_console)
+        self.console_btn = tkinter.Button(self,text="串口",command=lambda:self.chg_console())
         self.console_btn.pack()
 
         self.mainface = tkinter.Label(self,text="欢迎使用！")
         self.mainface.pack()
+
+        self.frame_1 = TelnetInterface(self)
+        self.frame_2 = ConsoleInterface(self)
 
 
 #telnet界面类
@@ -39,6 +52,8 @@ class TelnetInterface(tkinter.Frame):
         super().__init__()
         self.ip_address = tkinter.Label(self,text="IP地址")
         self.ip_address.pack()
+        self.telnet_text = tkinter.Label(self,text="我是telnet界面")
+        self.telnet_text.pack()
 
 
 
@@ -50,9 +65,12 @@ class ConsoleInterface(tkinter.Frame):
         super().__init__()
         self.console_port = tkinter.Label(self,text="COM口")
         self.console_port.pack()
+        self.console_text = tkinter.Label(self,text="我是Console界面")
+        self.console_text.pack()
 
 
 
 base = Application(root)
 print(base.winfo_toplevel())
+print(base.__dict__)
 base.mainloop()
