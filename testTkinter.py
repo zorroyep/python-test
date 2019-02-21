6e23815e42a8b68b6697914a503b7223273c9e1d
zorroyep <wds.1990@163.com>
2019-02-21 11:23:16 +0800

    把基本界面已经做出来了

--- testTkinter.py
+++ testTkinter.py
@@ -1,59 +1,80 @@
 # -*- coding:utf-8 -*-
 
 import tkinter
+#import sys
 
-root = tkinter.Tk()
 
 #建立一个主界面类
 #
 class Application(tkinter.Frame):
     def chg_telnet(self):
-        if self.frame_2:
+        print("开始执行chg_telnet")
+        if self.frame_2.winfo_exists():
             self.frame_2.destroy()
-        elif self.frame_1:
-            self.frame_1.pack()
+            #print(sys.getrefcount(self.frame_2))
+            print("开始判断1")
+            #self.frame_1 = TelnetInterface(self)
+            #self.frame_1.pack()
+        if self.frame_1.winfo_exists():
+            print("开始判断2")
+            #self.frame_1.destroy()
+            self.frame_1.grid(row=2,column=0)
         else:
+            print("开始判断3")
             self.frame_1 = TelnetInterface(self)
-            self.frame_1.pack()
+            self.frame_1.grid(row=2,column=0)
 
         
 
     def chg_console(self):
-        if self.frame_1:
+        print("开始执行console_telnet")
+        if self.frame_1.winfo_exists():
+            print("开始判断1")
             self.frame_1.destroy()
-        elif self.frame_2:
-            #self.frame_2 = ConsoleInterface(self)
-            self.frame_2.pack()
+        if self.frame_2.winfo_exists():
+            print("开始判断2")
+            #self.frame_2.destroy()
+            self.frame_2.grid(row=2,column=0)
         else:
+            print("开始判断3")
             self.frame_2 = ConsoleInterface(self)
-            self.frame_2.pack()
+            self.frame_2.grid(row=2,column=0)
         
 
 
     def __init__(self,master):
         super().__init__()
-        self.pack()
+        self.grid()
+        
         self.telnet_btn = tkinter.Button(self,text="Telnet",command=lambda:self.chg_telnet())
-
-        self.telnet_btn.pack()
+        self.telnet_btn.grid(row=0,column=0,sticky=tkinter.NW)
         self.console_btn = tkinter.Button(self,text="串口",command=lambda:self.chg_console())
-        self.console_btn.pack()
+        self.console_btn.grid(row=0,column=1,sticky=tkinter.NW)
 
-        self.mainface = tkinter.Label(self,text="欢迎使用！")
-        self.mainface.pack()
+        #self.mainface = tkinter.Label(self,text="欢迎使用！")
+        #self.mainface.pack()
 
         self.frame_1 = TelnetInterface(self)
+        self.frame_1.grid(row=2,column=0)
         self.frame_2 = ConsoleInterface(self)
+        
+        #self.destroy()
+
+        
 
 
 #telnet界面类
 class TelnetInterface(tkinter.Frame):
     def __init__(self,parent):
         super().__init__()
-        self.ip_address = tkinter.Label(self,text="IP地址")
-        self.ip_address.pack()
         self.telnet_text = tkinter.Label(self,text="我是telnet界面")
-        self.telnet_text.pack()
+        self.telnet_text.grid(row=3,column=0,sticky=tkinter.NW)
+        self.ip_address = tkinter.Label(self,text="IP地址")
+        self.ip_address.grid(row=4,column=0,sticky=tkinter.NW)
+        #IP地址输入栏
+        #打印回显栏
+        #添加一些按钮
+        
 
 
 
@@ -63,14 +84,22 @@ class TelnetInterface(tkinter.Frame):
 class ConsoleInterface(tkinter.Frame):
     def __init__(self,parent):
         super().__init__()
-        self.console_port = tkinter.Label(self,text="COM口")
-        self.console_port.pack()
         self.console_text = tkinter.Label(self,text="我是Console界面")
-        self.console_text.pack()
+        self.console_text.grid(row=3,column=0,sticky=tkinter.NW)
+        self.console_port = tkinter.Label(self,text="COM口")
+        self.console_port.grid(row=4,column=0,sticky=tkinter.NW)
+        #连接串口所需要的参数
+        #1，串口波特率，端口号，数据位，校验，停止位，流控
+
+        #
+        
 
 
 
-base = Application(root)
-print(base.winfo_toplevel())
-print(base.__dict__)
-base.mainloop()
+root = tkinter.Tk()
+root.title("串口小工具 v1.0")
+root.geometry("500x400")
+baseDesk = Application(root)
+print(baseDesk.winfo_toplevel())
+#print(dir(baseDesk))
+baseDesk.mainloop()
